738e9c6b2de483b6961b2301bdeffb19f981008c
jacquestrdx123 <jacques.trdx@gmail.com>
2023-04-04 11:23:12 +0200

    Second Commit

--- app/Models/Scan.php
+++ app/Models/Scan.php
@@ -9,5 +9,21 @@ class Scan extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['created_at','updated_at','code'];
+    protected $fillable = [
+        'created_at',
+        'updated_at',
+        'code',
+        'current_state',
+        'pn_number',
+        'order_time',
+        'picking_time',
+        'confirmation_time',
+        'invoice_time',
+        'invoice_number',
+        'loading_registration',
+        'loading_time  ',
+        'security_registration',
+        'security_time',
+        'pod_time'
+    ];
 }
