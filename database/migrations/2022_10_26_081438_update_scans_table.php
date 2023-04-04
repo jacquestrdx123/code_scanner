7671878e0fb5b4deeb993cfc1d4ef5e3ea9fc6a0
jacquestrdx123 <jacques.trdx@gmail.com>
2023-04-04 11:19:19 +0200

    Second Commit

--- database/migrations/2022_10_26_081438_update_scans_table.php
+++ database/migrations/2022_10_26_081438_update_scans_table.php
@@ -13,16 +13,42 @@ return new class extends Migration
      */
     public function up()
     {
-        if (Schema::hasColumn('scans', 'order_nr')){
+            Schema::table('scans', function (Blueprint $table) {
+                $table->string('current_state')->nullable();
+            });
             Schema::table('scans', function (Blueprint $table) {
                 $table->string('order_nr')->nullable();
             });
-        }
-        if (Schema::hasColumn('scans', 'start_pick')){
             Schema::table('scans', function (Blueprint $table) {
-                $table->string('order_nr')->nullable();
+                $table->string('invoice_number')->nullable();
             });
-        }
+            Schema::table('scans', function (Blueprint $table) {
+                $table->string('loading_registration')->nullable();
+            });
+            Schema::table('scans', function (Blueprint $table) {
+                $table->string('security_registration')->nullable();
+            });
+            Schema::table('scans', function (Blueprint $table) {
+                $table->timestamp('order_time')->nullable();
+            });
+            Schema::table('scans', function (Blueprint $table) {
+                $table->timestamp('picking_time')->nullable();
+            });
+            Schema::table('scans', function (Blueprint $table) {
+                $table->timestamp('confirmation_time')->nullable();
+            });
+            Schema::table('scans', function (Blueprint $table) {
+                $table->timestamp('invoice_time')->nullable();
+            });
+            Schema::table('scans', function (Blueprint $table) {
+                $table->timestamp('loading_time')->nullable();
+            });
+            Schema::table('scans', function (Blueprint $table) {
+                $table->timestamp('security_time')->nullable();
+            });
+            Schema::table('scans', function (Blueprint $table) {
+                $table->timestamp('pod_time')->nullable();
+            });
     }
 
     /**
