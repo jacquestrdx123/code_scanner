a005dd621592180c44f7e78365a232445b1cb50b
jacquestrdx123 <jacques.trdx@gmail.com>
2023-04-04 13:16:32 +0200

    Second Commit

--- resources/views/update_scan.blade.php
+++ resources/views/update_scan.blade.php
@@ -93,7 +93,7 @@
                                     <div class="flex-col justify-center">
                                         <label for="pn_number" class="text-center block text-sm font-medium text-gray-700">Invoice Number</label>
                                         <div class="flex mt-1">
-                                            <input type="text" autofocus name="order_number" id="order_number"
+                                            <input type="text" autofocus name="invoice_number" id="invoice_number"
                                                    class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                                                    placeholder="Invoice Here">
                                         </div>
@@ -102,7 +102,6 @@
                                 <input style="display:none" type="text" autofocus name="scan_id" id="scan_id"
                                        class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                                        value="{!! $scan->id !!}">
-                                <button type="submit" class="button btn-primary btn">Submit</button>
 
                             </form>
                     </div>
