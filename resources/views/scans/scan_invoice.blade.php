9a2569785430244e9d2b89ae7d529a5c53619d9c
Jacquestrdx123 <jacques.trdx@gmail.com>
2023-05-08 14:24:31 +0200

    Second Commit

--- resources/views/scans/scan_invoice.blade.php
+++ resources/views/scans/scan_invoice.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
 </head>
 <body>
 
