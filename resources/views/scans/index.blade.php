60d9320c32cc0b24c023263ae9dcd5352501d7d9
Jacquestrdx123 <jacques.trdx@gmail.com>
2023-05-09 06:56:27 +0200

    Second Commit

--- resources/views/scans/index.blade.php
+++ resources/views/scans/index.blade.php
@@ -62,9 +62,14 @@
         <div class="sm:hidden" id="mobile-menu">
             <div class="space-y-1 pt-2 pb-3">
                 <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" -->
-                <a href="#" class="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium" aria-current="page">Scanner</a>
-
-                <a href="#" class="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">Exporter</a>
+                <a href="/scan/1" class="border-transparent text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium" aria-current="page">Order</a>
+                <a href="/scan/2" class="border-transparent text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium" aria-current="page">Picking</a>
+                <a href="/scan/3" class="border-transparent text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium" aria-current="page">Confirmation</a>
+                <a href="/scan/4" class="border-transparent text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium" aria-current="page">Invoice</a>
+                <a href="/scan/5" class="border-transparent text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium" aria-current="page">Loading</a>
+                <a href="/scan/6" class="border-transparent text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium" aria-current="page">Security</a>
+                <a href="/scan/7" class="border-transparent text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium" aria-current="page">POD</a>
+                <a href="/scan" class="border-indigo-500 text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">Exporter</a>
 
             </div>
         </div>
