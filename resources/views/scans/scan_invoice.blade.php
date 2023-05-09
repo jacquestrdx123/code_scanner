61cb855ee7a988f481d801e27ecd64f56da6815b
Jacquestrdx123 <jacques.trdx@gmail.com>
2023-05-09 07:31:45 +0200

    Second Commit

--- resources/views/scans/scan_invoice.blade.php
+++ resources/views/scans/scan_invoice.blade.php
@@ -68,7 +68,7 @@
         <div class="sm:hidden" id="mobile-menu">
             <div class="space-y-1 pt-2 pb-3">
                 <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" -->
-                <a href="#" class="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium" aria-current="page">Scanner</a>
+                <a href="#" class="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium" aria-current="page">Scanner: <b>Invoice</b></a>
 
                 <a href="#" class="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">Exporter</a>
 
