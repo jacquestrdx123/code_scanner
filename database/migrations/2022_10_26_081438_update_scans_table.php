c876090f77bc662bb67ab7d31dc04e4d5e914637
Jacquestrdx123 <jacques.trdx@gmail.com>
2023-05-19 10:24:04 +0200

    Second Commit

---
+++ database/migrations/2022_10_26_081438_update_scans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('scans', function (Blueprint $table) {
+            $table->string('picking_name')->nullable();
+            $table->string('confirm_name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scans');
+    }
+};
